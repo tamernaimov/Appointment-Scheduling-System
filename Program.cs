0d2d6b1ffc47658432d92240793e6e9cd1b6d3d5
tamernaimov <tamer.naimov@gmail.com>
2026-03-24 21:10:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Appointment_Scheduling_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
