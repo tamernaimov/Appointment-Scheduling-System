935a1361d03fbf0a23a8a81bb05318a44982c381
Dzhem10 <hakadzhem@gmail.com>
2026-03-31 23:05:47 +0300

    Added Appoinment and Client

--- Domain/Entities/Appointment.cs
+++ Domain/Entities/Appointment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Appointment_Scheduling_System.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace Appointment_Scheduling_System.Domain.Entities
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int Id { get; set; }
+
+        public int ClientId { get; set; }
+
+        public int StaffId { get; set; }
+
+        public int ServiceId { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        //public AppointmentStatus Status { get; set; }
     }
 }
--- Domain/Entities/Client.cs
+++ Domain/Entities/Client.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Appointment_Scheduling_System.Domain.Entities
 {
-    internal class Client
-    {
+    public class Client
+    {   
+       public int Id { get; set; }
+
+       public string FirstName { get; set; }
+
+       public string LastName { get; set; }
+
+       public string PhoneNumber { get; set; }
+
+       public string Email { get; set; }
+        
     }
 }
