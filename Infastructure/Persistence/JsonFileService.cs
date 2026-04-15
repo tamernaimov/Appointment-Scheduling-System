c489c43f6a8658e87dc6316ace0e589f1d23ea66
Dzhem10 <hakadzhem@gmail.com>
2026-04-16 00:27:12 +0300

    Addded Json Reading and Writing

---
+++ Infastructure/Persistence/JsonFileService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace Appointment_Scheduling_System.Infrastructure.Persistence
+{
+    public class JsonFileService
+    {
+        public List<T> Read<T>(string filePath)
+        {
+            if (!File.Exists(filePath))
+                return new List<T>();
+
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+        }
+
+        public void Write<T>(string filePath, List<T> data)
+        {
+            var json = JsonSerializer.Serialize(data, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
