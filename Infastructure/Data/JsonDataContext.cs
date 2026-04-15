b39a42d1641912bed2afea8046679d2f7d3e172f
Dzhem10 <hakadzhem@gmail.com>
2026-04-16 00:32:04 +0300

    Added Saving with JSON

---
+++ Infastructure/Data/JsonDataContext.cs
@@ -0,0 +1,30 @@
+﻿using Appointment_Scheduling_System.Domain.Entities;
+using Appointment_Scheduling_System.Infrastructure.Persistence;
+
+namespace Appointment_Scheduling_System.Infrastructure.Data
+{
+    public class JsonDataContext
+    {
+        private readonly JsonFileService _fileService;
+
+        public List<Client> Clients { get; set; }
+        public List<Appointment> Appointments { get; set; }
+
+        private readonly string clientsFile = "Data/clients.json";
+        private readonly string appointmentsFile = "Data/appointments.json";
+
+        public JsonDataContext(JsonFileService fileService)
+        {
+            _fileService = fileService;
+
+            Clients = _fileService.Read<Client>(clientsFile);
+            Appointments = _fileService.Read<Appointment>(appointmentsFile);
+        }
+
+        public void SaveChanges()
+        {
+            _fileService.Write(clientsFile, Clients);
+            _fileService.Write(appointmentsFile, Appointments);
+        }
+    }
+}
--- Infastructure/Repositories/ClientRepository.cs
+++ Infastructure/Repositories/ClientRepository.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Appointment_Scheduling_System.Application.Interfaces;
+using Appointment_Scheduling_System.Domain.Entities;
+using Appointment_Scheduling_System.Infrastructure.Data;
 
-namespace Appointment_Scheduling_System.Infastructure.Repositories
+namespace Appointment_Scheduling_System.Infrastructure.Repositories
 {
-    internal class ClientRepository
+    public class ClientRepository : IClientRepository
     {
+        private readonly JsonDataContext _context;
+
+        public ClientRepository(JsonDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Client client)
+        {
+            client.Id = _context.Clients.Count + 1;
+            _context.Clients.Add(client);
+            _context.SaveChanges();
+        }
+
+        public List<Client> GetAll()
+        {
+            return _context.Clients;
+        }
+
+        public Client GetById(int id)
+        {
+            return _context.Clients.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Client client)
+        {
+            var existing = GetById(client.Id);
+            if (existing == null) return;
+
+            existing.FirstName = client.FirstName;
+            existing.LastName = client.LastName;
+            existing.PhoneNumber = client.PhoneNumber;
+            existing.Email = client.Email;
+
+            _context.SaveChanges();
+        }
     }
 }
