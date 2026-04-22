8bc1370defb9a0df9e99008eebe6a58e40d3ece4
Dzhem10 <hakadzhem@gmail.com>
2026-04-22 09:37:17 +0300

    Update StaffRepository.cs

--- Infastructure/Repositories/StaffRepository.cs
+++ Infastructure/Repositories/StaffRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Appointment_Scheduling_System.Domain.Entities;
+using Appointment_Scheduling_System.Infrastructure.Data;
 
-namespace Appointment_Scheduling_System.Infastructure.Repositories
+namespace Appointment_Scheduling_System.Infrastructure.Repositories
 {
-    internal class StaffRepository
+    public class StaffRepository
     {
+        private readonly JsonDataContext _context;
+
+        public StaffRepository(JsonDataContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Staff staff)
+        {
+            staff.Id = _context.Staff.Count + 1;
+            _context.Staff.Add(staff);
+            _context.SaveChanges();
+        }
+
+        public List<Staff> GetAll()
+        {
+            return _context.Staff;
+        }
+
+        public Staff GetById(int id)
+        {
+            return _context.Staff.FirstOrDefault(s => s.Id == id);
+        }
     }
 }
