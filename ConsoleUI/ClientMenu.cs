fce0077cc9ebed4b4d2fd98618b022b6fc350866
Dzhem10 <hakadzhem@gmail.com>
2026-04-27 22:50:27 +0300

    Added Menu - can add appintments and clients

--- ConsoleUI/ClientMenu.cs
+++ ConsoleUI/ClientMenu.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Appointment_Scheduling_System.Application.Services;
 
-namespace Appointment_Scheduling_System.ConsoleUI
+namespace Appointment_Scheduling_System.ConsoleUI.Menus
 {
-    internal class ClientMenu
+    public class ClientMenu
     {
+        private readonly ClientService _clientService;
+
+        public ClientMenu(ClientService clientService)
+        {
+            _clientService = clientService;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n--- Clients ---");
+            Console.WriteLine("1. Add client");
+            Console.WriteLine("2. List clients");
+
+            var choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Write("First name: ");
+                var firstName = Console.ReadLine();
+
+                Console.Write("Last name: ");
+                var lastName = Console.ReadLine();
+
+                Console.Write("Phone: ");
+                var phone = Console.ReadLine();
+
+                Console.Write("Email: ");
+                var email = Console.ReadLine();
+
+                _clientService.CreateClient(firstName, lastName, phone, email);
+            }
+            else if (choice == "2")
+            {
+                var clients = _clientService.GetAllClients();
+
+                foreach (var c in clients)
+                {
+                    Console.WriteLine($"{c.Id}: {c.FirstName} {c.LastName}");
+                }
+            }
+        }
     }
 }
