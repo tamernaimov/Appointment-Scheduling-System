eeaf6ebbed3423ee4c1e83e1419dba7b62f3249e
tamernaimov <tamer.naimov@gmail.com>
2026-06-15 22:27:04 +0300

    Fixed errors

--- ConsoleUI/MainMenu.cs
+++ ConsoleUI/MainMenu.cs
@@ -1,26 +1,43 @@
-﻿using Appointment_Scheduling_System.Application.Services;
-using Appointment_Scheduling_System.Application.Interfaces;
-using Appointment_Scheduling_System.Infrastructure.Repositories;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Appointment_Scheduling_System.Application.Interfaces;
+using Appointment_Scheduling_System.ConsoleUI.Menus;
 
 namespace Appointment_Scheduling_System.ConsoleUI.Menus
 {
     public class MainMenu
     {
-       
-        private readonly IServiceProvider _provider;
+        private readonly ClientMenu _clientMenu;
+        private readonly ServiceMenu _serviceMenu;
+        private readonly StaffMenu _staffMenu;
+        private readonly AppointmentMenu _appointmentMenu;
+        private readonly ReportMenu _reportMenu;
+        private readonly ScheduleMenu _scheduleMenu;
 
-        public MainMenu(IServiceProvider provider)
+        public MainMenu(
+            ClientMenu clientMenu,
+            ServiceMenu serviceMenu,
+            StaffMenu staffMenu,
+            AppointmentMenu appointmentMenu,
+            ReportMenu reportMenu,
+            ScheduleMenu scheduleMenu)
         {
-            _provider = provider;
+            _clientMenu = clientMenu;
+            _serviceMenu = serviceMenu;
+            _staffMenu = staffMenu;
+            _appointmentMenu = appointmentMenu;
+            _reportMenu = reportMenu;
+            _scheduleMenu = scheduleMenu;
         }
+
         public void Show()
         {
             while (true)
             {
                 Console.Clear();
 
-                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("========================================");
+                Console.WriteLine("   APPOINTMENT SCHEDULING SYSTEM");
+                Console.WriteLine("========================================");
+
                 Console.WriteLine("[1] Clients");
                 Console.WriteLine("[2] Services");
                 Console.WriteLine("[3] Staff");
@@ -29,32 +46,33 @@ namespace Appointment_Scheduling_System.ConsoleUI.Menus
                 Console.WriteLine("[6] Schedule");
                 Console.WriteLine("[0] Exit");
 
+                Console.Write("Choose: ");
                 var choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
-                        _provider.GetRequiredService<ClientMenu>().Show();
+                        _clientMenu.Show();
                         break;
 
                     case "2":
-                        _provider.GetRequiredService<ServiceMenu>().Show();
+                        _serviceMenu.Show();
                         break;
 
                     case "3":
-                        _provider.GetRequiredService<StaffMenu>().Show();
+                        _staffMenu.Show();
                         break;
 
                     case "4":
-                        _provider.GetRequiredService<AppointmentMenu>().Show();
+                        _appointmentMenu.Show();
                         break;
 
                     case "5":
-                        _provider.GetRequiredService<ReportMenu>().Show();
+                        _reportMenu.Show();
                         break;
 
                     case "6":
-                        _provider.GetRequiredService<ScheduleMenu>().Show();
+                        _scheduleMenu.Show();
                         break;
 
                     case "0":
